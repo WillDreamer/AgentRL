031f7dcad0af4cf25d500d6fbeff58449cc78339
root <root@ip-10-3-224-41.ap-south-1.compute.internal>
2025-08-12 18:13:23 +0000

    first

---
+++ setup_env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 初始化 conda（source conda 的 profile）
+eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
+
+conda create -n verl python==3.11 -y
+conda init
+conda activate verl
+python3 -m pip install uv
+
+python3 -m uv pip install -e ".[sglang]"
+pip install --no-deps -e .
+python3 -m uv pip install flash-attn==2.7.4.post1 --no-build-isolation --no-deps
+python3 -m uv pip install -r ./requirements.txt
