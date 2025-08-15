336c7a89ae6c0a43bbc703a1869ea50febe13a49
root <root@ip-10-1-98-138.ap-south-1.compute.internal>
2025-08-15 04:14:35 +0000

    add:webshop

--- recipe/webshop/setup_webshop.sh
+++ recipe/webshop/setup_webshop.sh
@@ -18,15 +18,15 @@ git clone https://github.com/ZihanWang314/webshop-minimal.git
 
 # webshop installation, model loading
 pip install -e webshop-minimal/ --no-dependencies
-python3 -m uv pip install -U "numpy==2.3.2" "scipy==1.16.1"
+pip install -U "numpy==2.3.2" "scipy==1.16.1"
 python -m spacy download en_core_web_sm
 python -m spacy download en_core_web_lg
-python3 -m uv pip install -U "numpy==2.2.6"
+pip install -U "numpy==2.2.6"
 
 conda install conda-forge::gdown -y
 mkdir -p webshop-minimal/webshop_minimal/data/full
 cd webshop-minimal/webshop_minimal/data/full
-gdown https://drive.google.com/uc?id=1A2whVgOO0euk5O13n2iYDM0bQRkkRduB # items_shuffle
+gdown https://drive.google.com/uc?id=1Yk4hsehueKuWwDCCsUOyyEpqUkzrmUu1 # items_shuffle
 gdown https://drive.google.com/uc?id=1s2j6NgHljiZzQNL3veZaAiyW_qDEgBNi # items_ins_v2
 cd ../../../..
 
