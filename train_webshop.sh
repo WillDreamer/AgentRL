8b1f6d61919d36ddc3b62a2ecb5765d1647a99b4
Haixin Wang <whx@cs.ucla.edu>
2025-08-19 22:14:34 -0700

    Update train_webshop.sh

--- train_webshop.sh
+++ train_webshop.sh
@@ -1,8 +1,9 @@
 set -e
 
 TASK_NAME=_webshop
+REMARK=prompt_v2
 FILTER_RATIO=0.5
-MODEL=Qwen/Qwen3-4B-Thinking-2507
+MODEL=Qwen/Qwen3-1.7B
 
 WANDB_API_KEY="ba70fcbc92808cc7a1750dd80ac3908295e6854f" # Modify your wandb key
 # ============================ Preparation ============================
@@ -16,6 +17,6 @@ python -m recipe.webshop.main_webshop --config-name $TASK_NAME \
     model_path=$MODEL \
     system.CUDA_VISIBLE_DEVICES=\"0,1,2,3,4,5,6,7\" \
     actor_rollout_ref.rollout.rollout_filter_ratio=$FILTER_RATIO \
-    trainer.experiment_name=$TASK_NAME-ppo-rolloutfilter$FILTER_RATIO-$MODEL \
+    trainer.experiment_name=$TASK_NAME-$REMARK-ppo-filter$FILTER_RATIO-$MODEL \
     trainer.nnodes=1 \
     
