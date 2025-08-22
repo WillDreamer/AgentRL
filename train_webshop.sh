0744f92d9d02bdcfdc42f75ead4ff07c83db886d
Haixin Wang <whx@cs.ucla.edu>
2025-08-21 19:03:41 -0700

    Update train_webshop.sh

--- train_webshop.sh
+++ train_webshop.sh
@@ -1,9 +1,9 @@
 set -e
 
 TASK_NAME=_webshop
-REMARK=prompt_v2
-FILTER_RATIO=0.5
-MODEL=Qwen/Qwen3-1.7B
+REMARK=prompt_v2_T_5_Len_720
+FILTER_RATIO=0.25
+MODEL=Qwen/Qwen3-4B
 
 WANDB_API_KEY="ba70fcbc92808cc7a1750dd80ac3908295e6854f" # Modify your wandb key
 # ============================ Preparation ============================
@@ -19,5 +19,7 @@ python -m recipe.webshop.main_webshop --config-name $TASK_NAME \
     actor_rollout_ref.rollout.rollout_filter_ratio=$FILTER_RATIO \
     trainer.experiment_name=$TASK_NAME-$REMARK-ppo-filter$FILTER_RATIO-$MODEL \
     trainer.nnodes=1 \
-    trainer.rollout_data_dir=log_rollout
+    trainer.rollout_data_dir=log_rollout \
+    agent_proxy.max_turn=5 \
+    actor_rollout_ref.rollout.response_length=720 \
     
