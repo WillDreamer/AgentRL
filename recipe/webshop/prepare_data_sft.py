d4e828c9257375a700d46bc8cc2b0fca10814a0b
Haixin Wang <whx@cs.ucla.edu>
2025-09-01 19:10:20 -0700

    Update prepare_data_sft.py

--- recipe/webshop/prepare_data_sft.py
+++ recipe/webshop/prepare_data_sft.py
@@ -11,6 +11,7 @@ from typing import List, Dict
 from verl.protocol import pad_dataproto_to_divisor, unpad_dataproto
 from recipe.webshop.llm_agent.base_llm import ConcurrentLLM
 from recipe.webshop.llm_agent.agent_proxy_bedrock import LLMAgentProxy_Bedrock
+import json
 from api.bedrock import get_model
 
 @hydra.main(version_base=None, config_path="./config", config_name="base_bedrock")
@@ -19,9 +20,13 @@ def main(config):
 	os.environ["VLLM_WORKER_MULTIPROC_METHOD"] = "spawn"
 	os.environ["CUDA_VISIBLE_DEVICES"] = str(config.system.CUDA_VISIBLE_DEVICES)
 
-	model_name = 'anthropic.claude-3-5-sonnet-20240620-v1:0'
-	region = 'us-east-1'
-	model = get_model(model_name, None, region)
+	# model_name = 'anthropic.claude-3-5-sonnet-20240620-v1:0'
+	model_name = 'openai/gpt-oss-20b'
+	if 'claude' in model_name:
+		region = 'us-east-1'
+		model = get_model(model_name, None, region)
+	else:
+		model = model_name
 	proxy = LLMAgentProxy_Bedrock(config, model)
 	import time
 	start_time = time.time()
@@ -29,7 +34,7 @@ def main(config):
 	success = metrics['WebShop/success']
 	# save rollout and success to json file
 	import json
-	with open('rollout_1500_2000.json', 'w') as f:
+	with open('rollout_1500_1800.json', 'w') as f:
 		json.dump({'rollout': rollouts, 'success': success}, f)
 
 
